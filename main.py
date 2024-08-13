68efd8b14c7cbe0b5d76be38b4458ba4f593abb9
Tanuj Kashyap <93467410+tanuj8124@users.noreply.github.com>
2024-08-13 22:39:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],   
-
-)
+    allow_headers=["*"])
 
 
 # Define a GET method that takes the three parameters as query parameters
